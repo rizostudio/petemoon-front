264bdbdb3d1ce91a47dea75e9bc97bc273ba2bcc
MehdiiRezakhani <mehdiirezakhani@gmail.com>
2022-12-26 16:09:01 +0330

    Tailwind config is done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,35 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '360px',
+      // => @media (min-width: 360px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      //main color
+      'primary':'#EA6352',
+      'secondary':'#DEDFE1',
+      'thirdly':'#9B9BA1',
+      'white':'#FFFFFF',
+      'black':'#191C27',
+      'info':'#4DA4F4',
+      'warning':'#EEC800',
+      'verify':'#2DB95D',
+      'error':'#CE0000',
+    },
+    fontFamily: {
+      primary: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
