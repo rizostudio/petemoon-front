229aa93f646096d74c867c0e509b333bf295f293
Amirhossein <amirabdolii@gmail.com>
2022-12-27 01:31:38 +0330

    feat: input and signup for mobile is up

--- components/common/input/index.jsx
+++ components/common/input/index.jsx
@@ -1,17 +1,36 @@
-export default function FloatLabelInput() {
+import { useState } from "react";
+
+export default function FloatLabelInput({
+  name,
+  value,
+  placeholder,
+  type,
+  onChange,
+  required,
+  py,
+  dir = "rtl",
+}) {
+  const [starShow, setStarShow] = useState(required);
   return (
-    <div class="relative">
+    <div className="relative w-full" dir={"rtl"}>
       <input
-        type="text"
-        id="floating_outlined"
-        class="block px-2.5 py-2 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-[#9B9BA1] appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+        type={type}
+        id={name}
+        className={`block bg-white px-3 w-full text-sm text-[#333333] bg-transparent rounded-xl border border-[#9B9BA1] appearance-none focus:outline-none focus:ring-0 focus:border-primary peer py-${py}`}
         placeholder=" "
+        onChange={onChange}
+        value={value}
+        name={name}
+        onClick={() => setStarShow(false)}
+        onBlur={() => setStarShow(true)}
       />
       <label
-        for="floating_outlined"
-        class="absolute text-sm text-[#9B9BA1] duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 focus:right-0 right-1"
+        htmlFor={name}
+        className="absolute left-auto text-sm text-[#9B9BA1] duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white peer-focus:bg-white px-2 peer-focus:px-1 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 right-1 peer-focus:right-0 secondPeer"
+        dir="rtl"
       >
-        Floating outlined
+        {placeholder}
+        {required && starShow && "*"}
       </label>
     </div>
   );
--- pages/sign-up.jsx
+++ pages/sign-up.jsx
@@ -11,18 +11,17 @@ export default function signUp() {
       email: "",
       refCode: "",
     },
-    onSubmit: () => {},
+    onSubmit: (value) => {
+      console.log(value);
+    },
   });
   return (
-    <div className="flex flex-col h-screen w-full bg-primaryMobileBg py-10 px-5 relative">
-      <Image src={backgroundLines} className="absolute bottom-0 left-3" />
+    <div className="flex flex-col h-screen w-full bg-primaryMobileBg p-5 relative">
+      <Image src={backgroundLines} className="absolute bottom-0 left-0" />
       <div id="image" className="hidden"></div>
-      <div className="flex flex-col items-center pt-20 space-x-8">
+      <div className="flex flex-col h-full items-center pt-20 space-y-8 z-10">
         <div className="flex flex-col space-y-4">
-          <div
-            id="logo"
-            className="flex flex-col items-center text-primary"
-          >
+          <div id="logo" className="flex flex-col items-center text-primary">
             <Image src={signupLogo} width={61} height={61} />
             <p className="font-bold text-xl">petemoon</p>
           </div>
@@ -30,9 +29,49 @@ export default function signUp() {
             کاربر عزیز خوش آمدید
           </div>
         </div>
-        <div id="form">
-          <FloatLabelInput />
-        </div>
+        <form onSubmit={formik.handleSubmit} className="w-full h-full">
+          <div className="flex flex-col h-full items-center justify-between">
+            <FloatLabelInput
+              type={"text"}
+              placeholder={"نام"}
+              name="firstname"
+              onChange={formik.handleChange}
+              value={formik.values.firstname}
+              required={true}
+              py={3.5}
+            />
+            <FloatLabelInput
+              type={"text"}
+              placeholder={"نام خانوادگی"}
+              name="lastname"
+              onChange={formik.handleChange}
+              value={formik.values.lastname}
+              required={true}
+              py={3.5}
+            />
+            <FloatLabelInput
+              type={"email"}
+              placeholder={"ایمیل"}
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              required={false}
+              py={3.5}
+            />
+            <FloatLabelInput
+              type={"text"}
+              placeholder={"کد معرف"}
+              name="refCode"
+              onChange={formik.handleChange}
+              value={formik.values.refCode}
+              required={false}
+              py={3.5}
+            />
+            <button type="submit" className="btn bg-primary active:bg-primary focus:bg-primary w-full mt-6 rounded-lg">
+              click
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
