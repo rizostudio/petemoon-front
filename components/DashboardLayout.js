aeb20a6f10dbcf609f9a4c500eaeaddcbf306977
MehdiiRezakhani <mehdiirezakhani@gmail.com>
2023-01-01 05:36:55 +0330

    Wallet is completed!

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -25,15 +25,15 @@ const DashboardLayout = ({children}) => {
     const [minly, setMinly] = useState(false);
     const [notificationBar, setNotificationBar] = useState(true);
     const menuArr = [   {id:"my-profile", name:"حساب کاربری", icon:Profile_Icon},
-    {id:"addresses", name:"آدرس ها", icon:Address_Icon},
-    {id:"my-pet", name:"پت من", icon:MyPet_Icon},
-    {id:"wallet", name:"کیف پول", icon:Wallet_Icon},
-    {id:"orders", name:"سفارش ها", icon:Orders_Icon},
-    {id:"bookmarks", name:"علاقه مندی ها", icon:Favorite_Icon},
-    {id:"my-message", name:"پیام های من", icon:Message_Icon},
-    {id:"support", name:"پشتیبانی", icon:Help_Icon},
-    ]
-    const pageName = menuArr.find(item => `/dashboard/${item.id}` == router.asPath).name
+                        {id:"addresses", name:"آدرس ها", icon:Address_Icon},
+                        {id:"my-pet", name:"پت من", icon:MyPet_Icon},
+                        {id:"wallet", name:"کیف پول", icon:Wallet_Icon},
+                        {id:"orders", name:"سفارش ها", icon:Orders_Icon},
+                        {id:"bookmarks", name:"علاقه مندی ها", icon:Favorite_Icon},
+                        {id:"my-message", name:"پیام های من", icon:Message_Icon},
+                        {id:"support", name:"پشتیبانی", icon:Help_Icon},
+                    ]
+    const pageName = menuArr.find(item => router.asPath.includes(item.id))
     // if(typeof window !== "undefined") {
     //     let Drawer = document.getElementById('Drawer').style.display;
     //     let Inbox = document.getElementById('Inbox').style.display;
@@ -120,7 +120,7 @@ const DashboardLayout = ({children}) => {
             </div>
             <div id="Inbox" className='flex flex-col justify-between w-full '>
                 <div className='w-full bg-white hidden sm:flex flex-row-reverse justify-between items-center px-10 py-5 relative'>
-                        <Image src={ArrowLeft_Icon} alt="ArrowLeftIcon" onClick={() => setMinly(!minly)} className='bg-first w-10 h-10 p-3 rounded-full absolute top-[25%] right-[-2%]'/>
+                        <Image src={ArrowLeft_Icon} alt="ArrowLeftIcon" onClick={() => setMinly(!minly)} className='bg-[#eee] w-10 h-10 p-3 rounded-full absolute top-[25%] right-[-2%]'/>
                         <div className='flex flex-col items-end'>
                             <p className="text-black font-black text-2xl">خوش آمدی، علی عزیز</p>
                             <p className='text-black font-light text-base opacity-[0.9] leading-[30px]'>۰۹۳۰۱۲۳۴۵۶۷</p>
@@ -138,7 +138,7 @@ const DashboardLayout = ({children}) => {
                 </div>
                 <div className='bg-fourth w-full h-full p-10 sm:px-20 sm:py-12'>
                     <div className='w-full flex sm:hidden flex-row-reverse justify-between items-center mb-10'>
-                        <p className=' after:w-2 after:h-5 after:bg-first after:text-first after:content-["<"] after:ml-1 after:rounded-[2px]'>{pageName}</p>
+                        <p className=' after:w-2 after:h-5 after:bg-first after:text-first after:content-["<"] after:ml-1 after:rounded-[2px]'>{pageName && pageName.name}</p>
                         <div className='bg-first opacity-[0.8] p-4 rounded-[15px]'>
                             <Link href='/dashboard'>
                                 <Image src={ArrowLeftWhite_Icon} alt="ArrowIcon" className='w-full'/>
--- pages/dashboard/wallet.js
+++ pages/dashboard/wallet.js
@@ -6,6 +6,8 @@ import ArrowLeft_Icon from '../../asset/icons/Arrow-left-white.svg';
 import WalletAdd_Icon from '../../asset/icons/wallet-add.svg';
 import CloseButton_Icon from '../../asset/icons/close-button.svg';
 const wallet = () => {
+    const [increaseAmount, setIncreaseAmount] = useState()
+    console.log(increaseAmount)
     return (
         <DashboardLayout>
             <div className='w-full h-screen sm:h-auto flex flex-col sm:flex-row-reverse justify-between items-stretch'>
@@ -15,51 +17,44 @@ const wallet = () => {
                         <p className='self-center text-3xl font-black leading-16'><bdi>{"۲۵۲.۵۰۰"} تومان</bdi></p>
                         <p className='self-start text-[12px] font-medium leading-5'><bdi>*موجودی کیف پول قابل بازگردانی به حساب نیست</bdi></p>
                     </div>
-
                 </div>
                 <label htmlFor="add-wallet-modal" className='w-full sm:w-1/3 flex flex-row-reverse sm:flex-col-reverse justify-between sm:justify-center items-center px-10 sm:px-20 py-5 sm:py-8 sm:mr-5 bg-white border-[2px] solid border-first rounded-[12px] sm:rounded-[25px] cursor-pointer'>
                     <p className='text-2xl sm:text-base text-first font-bold leading-8 sm:mt-10'>افزایش موجودی</p>
                     <Image src={WalletAdd_Icon} alt="WalletAddPic" className='sm:w-40'/>
                 </label>
-            </div>
-                    <div className='bg-white px-10 py-4 rounded-[20px]'>
-                                <div className='w-full flex flex-row-reverse justify-between items-center mb-10'>
-                                <p className='text-xs sm:text-base text-black font-medium sm:font-black leading-8 after:hidden sm:after:inline-block after:w-2 after:h-4 after:bg-first after:text-first after:content-[""] after:ml-2 after:align-middle after:rounded-[2px]'>افزایش موجودی</p>
-                                <label htmlFor="add-wallet-modal" className="">
-                                    <Image src={CloseButton_Icon} alt="CloseIcon"/>
-                                    </label>
-                                </div>
-                    </div>
-                    <form>
-                        <div className='flex flex-col items-stretch justify-center p-4 sm:p-15'>
-                            <p className='text-base sm:text-lg text-black text-center font-medium leading-8 mb-7'><bdi>مبلغ مورد نظر جهت افزایش موجودی را وارد نمایید:</bdi></p>
-                            <input type="number" placeholder='۵۰۰.۰۰۰ تومان' className='w-full p-3 mb-3 sm:mt-6 sm:mb-4 bg-white text-base text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px] focus:border-error focus:text-black after:content-["fu"] after:text-lg after:text-error'/>
-                            <div className='w-full flex '>
-                                <button className="w-full p-3 mb-3 bg-white text-sm text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px]"><bdi>{500000} تومان</bdi></button>
-                                <button className="w-full p-3 mb-3 mx-2 sm:mx-3 bg-white text-sm text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px]"><bdi>{500000} تومان</bdi></button>
-                                <button className="w-full p-3 mb-3 bg-white text-sm text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px] "><bdi>{500000} تومان</bdi></button>
-                            </div>
-                        </div>
-
-                        <div className='flex flex-row-reverse items-center justify-between w-full'>
-                            <button className='w-full text-sm text-white text-center font-semibold py-3 rounded-[5px] bg-[#4DA4F4] border-[2px] solid border-[#4DA4F4] ml-2'>پرداخت</button>
-                            <button className='w-full text-sm text-error text-center font-semibold py-3 rounded-[5px] bg-white border-[2px] solid border-error'>انصراف</button>
-                        </div>
-                    </form>
+            </div>                     
             <div>
                 <input type="checkbox" id="add-wallet-modal" className="modal-toggle" />
                 <label htmlFor="add-wallet-modal" className="modal cursor-pointer">
-                <label className="modal-box relative" htmlFor="add-wallet-modal">
-                    <div>
-                        <div className='w-full flex flex-row-reverse justify-between items-center mb-10'>
-                        <p className=' after:w-2 after:h-5 after:bg-first after:text-first after:content-["<"] after:ml-1 after:rounded-[2px]'>افزایش موجودی</p>
-                        <label htmlFor="add-wallet-modal" className="btn">x</label>
-                    </div>
-                    </div>
-                    <form>
-                        <h3>form</h3>
-                    </form>
-                </label>
+                    <label className="modal-box w-full absolute sm:relative inset-x-0 bottom-0 px-10 py-4 sm:p-8 mx-auto bg-white rounded-[20px]">
+                        <div className='w-full flex flex-row-reverse justify-between items-center'>
+                            <p className='text-base sm:text-base text-black font-medium sm:font-black leading-7 after:hidden sm:after:inline-block after:w-2 after:h-4 after:bg-first after:text-first after:content-[""] after:ml-2 after:align-middle after:rounded-[2px]'>افزایش موجودی</p>
+                            <label htmlFor="add-wallet-modal">
+                                <Image src={CloseButton_Icon} alt="CloseIcon"/>
+                            </label>
+                        </div>
+                        <form onSubmit={event => event.preventDefault()} className="flex flex-col">
+                            <div className='flex flex-col items-stretch justify-center p-4 sm:p-10'>
+                                <p className='text-base sm:text-lg text-black text-center font-medium leading-7 mb-7'><bdi>مبلغ مورد نظر جهت افزایش موجودی را وارد نمایید:</bdi></p>
+                                <input 
+                                    type="number" 
+                                    value={increaseAmount} 
+                                    onChange={event => setIncreaseAmount(event.target.value)}    
+                                    placeholder="100000"
+                                    className='w-full p-3 mb-2 sm:mb-3 bg-white text-base text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px] focus:border-error focus:text-black after:content-["fu"] after:text-lg after:text-error'
+                                />
+                                <div className='w-full flex mb-3'>
+                                    <button onClick={() => setIncreaseAmount(500000)} className="w-full p-3 bg-white text-sm text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px]"><bdi>{500000} تومان</bdi></button>
+                                    <button onClick={() => setIncreaseAmount(200000)} className="w-full p-3 mx-1 sm:mx-2 bg-white text-sm text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px]"><bdi>{200000} تومان</bdi></button>
+                                    <button onClick={() => setIncreaseAmount(100000)} className="w-full p-3 bg-white text-sm text-center text-gray font-medium border-[1px] solid border-third rounded-[12px] sm:rounded-[5px] "><bdi>{100000} تومان</bdi></button>
+                                </div>
+                            </div>
+                            <div className='flex flex-row-reverse items-center justify-between w-full sm:w-1/3 sm:self-left'>
+                                <button className='w-full text-sm text-white text-center font-semibold py-3 sm:py-2 rounded-[5px] bg-[#4DA4F4] border-[2px] solid border-[#4DA4F4] ml-2'>پرداخت</button>
+                                <label htmlFor="add-wallet-modal" className='w-full text-sm text-error text-center font-semibold py-3 sm:py-2 rounded-[5px] bg-white border-[2px] solid border-error'>انصراف</label>
+                            </div>
+                        </form>
+                    </label>
                 </label>
             </div>
         </DashboardLayout>
