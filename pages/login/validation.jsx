6e81c91f2c483b43ab965af8b29a6f7a639f0037
Amirhossein <amirabdolii@gmail.com>
2023-01-05 00:51:23 +0330

    bugfix: input text alignment resolved

--- pages/login/validation.jsx
+++ pages/login/validation.jsx
@@ -68,7 +68,7 @@ export default function Login() {
                   type={"text"}
                   id={"confirmationCode"}
                   className={`block text-center text-sm md:text-xl bg-white px-3 h-full w-full text-[#333333] rounded-xl border border-[#9B9BA1] appearance-none focus:outline-none focus:ring-0 focus:border-primary focus:border-2 peer`}
-                  placeholder="- - - - - -"
+                  placeholder="- - - -"
                   onChange={formik.handleChange}
                   value={
                     formik.values.confirmationCode === ""
@@ -81,7 +81,7 @@ export default function Login() {
                   htmlFor={"confirmationCode"}
                   className="absolute scale-75 peer-focus:scale-[85%] peer-placeholder-shown:scale-100   text-sm md:text-lg peer-focus:text-sm peer-focus:md:text-lg peer-placeholder-shown:text-sm peer-placeholder-shown:md:text-lg text-[#9B9BA1] duration-300 transform -translate-y-4 top-2 z-10 bg-white peer-focus:bg-white px-1 peer-placeholder-shown:px-1 peer-focus:pr-2 peer-focus:text-primary peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:-translate-y-4 right-4 peer-focus:right-4 peer-placeholder-shown:right-4 text-right origin-top-right"
                 >
-                  {"کد ۶ رقمی را وارد نمایید"}
+                  {"کد ۴ رقمی را وارد نمایید"}
                 </label>
               </div>
               <button
