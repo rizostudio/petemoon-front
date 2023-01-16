7eefba0f65527c1b2fcf0b1ceb4b799fc5582d23
Amirhossein <amirabdolii@gmail.com>
2023-01-16 15:55:14 +0330

    feat: all services written just not working!

--- services/dashboard/address.js
+++ services/dashboard/address.js
@@ -2,9 +2,9 @@ import { errorResponse, successResponse } from "@/dto";
 import api, { allowCookie } from "@/services";
 
 const GET_ADDRESS_API = "dashboard/address";
-export const getAddresses = () => {
+export const getAddresses = async () => {
   try {
-    const response = api.get(GET_ADDRESS_API, allowCookie());
+    const response = await api.get(GET_ADDRESS_API, allowCookie());
     console.log(response.data.data);
     // const domainData = domainAddress(response.data.data);
     // return successResponse(domainData);
