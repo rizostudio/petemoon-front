942efe03fd3e7c96bdd72a871cc96e181052ee95
hfazeli <hossein.fazeli.h7854@gmail.com>
2023-08-08 01:17:00 +0330

    done

--- components/cart/BasketItems.jsx
+++ components/cart/BasketItems.jsx
@@ -30,9 +30,9 @@ export default function BasketItems() {
   return (
     <div className="flex flex-col justify-center items-center px-10 lg:px-0 lg:relative mb-5 lg:mb-0">
       <div className="lg:m-5 w-full my-2 lg:my-1">
-        {state.basket?.length ? (
-          state.basket?.map((item, index) => (
-            <BasketItem key={item.id} item={item} index={index} />
+        {state?.basket?.length ? (
+          state?.basket?.map((item, index) => (
+            <BasketItem key={v4()} item={item} index={index} />
           ))
         ) : (
           <div className="w-full flex flex-row lg:flex-col justify-center items-start lg:items-center mt-10 lg:mt-2 py-5 lg:py-10 bg-primary lg:bg-white rounded-[12px] lg:rounded-[25px] shadow-shadowB">
@@ -60,7 +60,7 @@ export default function BasketItems() {
       </div>
       {/* Continue Box */}
       {state?.basket?.length && (
-        <div className="hidden lg:flex flex-col justify-between items-center w-1/3 p-5 bg-[#ea63521a] rounded-[15px] mr-auto  bottom-10 left-0">
+        <div className="hidden lg:flex flex-col justify-between items-center w-1/3 p-5 bg-[#ea63521a] rounded-[15px] absolute bottom-10 left-0">
           <div className="flex justify-between w-full">
             <p className="text-xl text-gray-400 font-normal leading-8">
               مجموع سبد خرید:
@@ -79,7 +79,7 @@ export default function BasketItems() {
           </div>
           <button
             onClick={handleSaveBasket}
-            className="text-base hover:bg-[#d85241] text-center text-white font-medium leading-7 bg-primary p-3 w-full rounded-[12px] mt-1"
+            className="text-base text-center text-white font-medium leading-7 bg-primary p-3 w-full rounded-[12px] mt-1"
           >
             ادامه
           </button>
